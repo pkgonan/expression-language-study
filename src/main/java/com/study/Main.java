a13032a3cb84f3da80b67b5b15180f4e3a001bdf
pkgonan <pkgonan@yanolja.com>
2019-05-03 22:43:04 +0900

    [Add] Default SPEL Usage

--- src/main/java/com/study/Main.java
+++ src/main/java/com/study/Main.java
@@ -1,17 +1,22 @@
 package com.study;
 
+import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 public class Main {
 
     public static void main(String[] args) {
         ExpressionParser parser = new SpelExpressionParser();
-        Expression expression = parser.parseExpression("'Hello World'.bytes.length");
+        Expression expression = parser.parseExpression("length() > 3");
 
-        int length = (Integer) expression.getValue();
+        String input = "TEST";
+        EvaluationContext context = new StandardEvaluationContext(input);
 
-        System.out.println(length);
+        Boolean result = expression.getValue(context, Boolean.class);
+
+        System.out.println(result);
     }
 }
